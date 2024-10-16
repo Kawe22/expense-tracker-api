e1e01503db06864a6cea96e381688ad3f42fb0e5
Kauê José <kauejose2211@gmail.com>
2024-10-16 16:42:38 -0300

    Initial commit

---
+++ expense-tracker-api/src/test/java/br/ufpb/dsc/expense_tracker_api/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dsc.expense_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
